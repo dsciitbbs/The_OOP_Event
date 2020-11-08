3ff516e32bbdce5b76eb88d189c0904d11c224d5
Nagsenwaghmare <64947831+Nagsenwaghmare@users.noreply.github.com>
2020-11-08 14:57:05 +0530

    Find my code here in class.java file

---
+++ class.java
@@ -0,0 +1,32 @@
+// hii, i am nagsen waghmare
+//here i created two classes with two diffrent methods  
+//and inside first  class named Bike declared main method in which i called the method  named changeSpeed
+
+class Bike{
+   String Size ;
+   int cost;
+    static void changeSpeed(){
+    System.out.println("cost");
+   }
+   public static void main(String args){
+       changeSpeed();
+       Bike s1 = new Bike();
+       s1.cost = 10000;
+        s1.Size = "Big";
+        System.out.println(s1.cost+" "+s1.Size);
+   }
+
+}
+class Car{
+   int Mileage;
+   int NumberofGears;
+   int MaxSpeed;
+     static void turn(){
+       
+         System.out.println("MaxSpeed");
+     }
+}
+
+// Answer to Q.1 ==
+// I learned how to create classes and how to declare methods in java
+// initialize and access objects in class;
